41e0cb7cc7e04a2bbae08bb1b82b223bc9dea7ac
Jan Schmutz <janschmutz@hotmail.de>
2021-12-03 15:27:15 +0100

    Update and rename d3p2NOTWORKINGMESSY.ps1 to d3p2MESSY.ps1

--- jan/day3/d3p2MESSY.ps1
+++ jan/day3/d3p2MESSY.ps1
@@ -21,7 +21,7 @@ while($oxy.Length -gt 1){
             if(!($binNrSplit[$count])){ $oxy[$i]=0 }
         }
     }elseif($pos[$count] -lt $oxy.Length/2){
-        for($j=0; $j -le $oxy.Length; $j++){
+        for($j=0; $j -lt $oxy.Length; $j++){
             $binNrSplit = [int[]](($oxy[$j] -split '') -ne '')
             if($binNrSplit[$count]){$oxy[$j]=0 }
         }
@@ -32,7 +32,7 @@ while($oxy.Length -gt 1){
 
 
 $count=0
-
+$pos = [int[]]::new(12)
 
 while($co2.Length -gt 1){
     MajX -arrMaj $co2 -x $count
@@ -51,17 +51,4 @@ while($co2.Length -gt 1){
     $count++
 }
 
-function bintodec($varBin){
-    for($i=$pos.Length; $i -ge 1; $i--){
-    $exp = $pos.Length - $i
-    if($pos[$i-1] -gt $halfRes){
-        $gammaRate += [Math]::Pow(2,$exp)
-    }
-    if($pos[$i-1] -lt $halfRes){
-        $epsilonRate += [Math]::Pow(2,$exp)
-    }
-}
-}
-
-Write-Host $co2
-Write-Host $oxy
+Write-Host $($([Convert]::ToInt32($co2,2))*$([Convert]::ToInt32($oxy,2)))
